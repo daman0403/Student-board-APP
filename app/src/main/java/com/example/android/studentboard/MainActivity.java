852d8cd09c619cc33cfdd7248e2ad96eae3aeb35
Daman Goyal <goyaldaman11111@gmail.com>
2022-07-09 01:28:48 +0530

    new files added

--- app/src/main/java/com/example/android/studentboard/MainActivity.java
+++ app/src/main/java/com/example/android/studentboard/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.android.studentboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginBtn = findViewById(R.id.btnLogin);
+       loginBtn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent intent = new Intent(MainActivity.this, StudentInfoActivity.class);
+               startActivity(intent);
+           }
+       });
     }
 }
